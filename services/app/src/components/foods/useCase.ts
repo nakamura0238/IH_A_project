7ade958250756faae0475be9c6e2d07ed66bb298
Firewood235 <keitaford@icloud.com>
2022-08-07 14:56:07 +0900

    仮の値入り(エラー削除）

--- services/app/src/components/foods/useCase.ts
+++ services/app/src/components/foods/useCase.ts
@@ -15,60 +15,65 @@ const foodsUseCase = () => {
     // 時刻
     const d = new Date("2020, 1, 2, 3, 4, 4");
 
-    // データベース一覧表示
-
-
-    return [new FoodEntity(-1, food.user_id, -1, -1, 
+    const foods = [new FoodEntity(-1, user_id, -1, -1, 
       "ビーフ", d, "rotten")];
+
+    return [...foods];
   };
 
 
   // 食品追加処理
   const foodAdd = async (food: FoodAdd) => {
 
-    // 新規食品情報の取得
-    const id = food.id;
-    const user_id = food.user_id;
-    const icon_id = food.icon_id;
-    const place_id = food.place_id;
-    const name = food.name;
-    const expiration_date = food.expiration_date;
-    const comment = food.comment;
+    // // 新規食品情報の取得
+    // const id = food.id;
+    // const user_id = food.user_id;
+    // const icon_id = food.icon_id;
+    // const place_id = food.place_id;
+    // const name = food.name;
+    // const expiration_date = food.expiration_date;
+    // const comment = food.comment;
+    
+    // 時刻(仮)
+    const d = new Date("2020, 1, 2, 3, 4, 4");
 
     // データベース登録
 
 
-    return new FoodEntity(food.id, food.user_id, food.icon_id, food.place_id, 
-      food.name, food.expiration_date, food.comment);
+    return new FoodEntity(-1, -1, -1, -1, 
+      "ビーフ", d, "rotten");
     };
 
 
     // 食材更新処理
     const foodUpdate = async (food: FoodUpdate) => {
   
-      // 更新食品情報の取得
-      const id = food.id;
-      const user_id = food.user_id;
-      const icon_id = food.icon_id;
-      const place_id = food.place_id;
-      const name = food.name;
-      const expiration_date = food.expiration_date;
-      const comment = food.comment;
+      // // 更新食品情報の取得
+      // const id = food.id;
+      // const user_id = food.user_id;
+      // const icon_id = food.icon_id;
+      // const place_id = food.place_id;
+      // const name = food.name;
+      // const expiration_date = food.expiration_date;
+      // const comment = food.comment;
+      
+    // 時刻(仮)
+    const d = new Date("2020, 1, 2, 3, 4, 4");
   
       // データベース更新
   
 
-      return new FoodEntity(food.id, food.user_id, food.icon_id, food.place_id, 
-        food.name, food.expiration_date, food.comment);
+      return new FoodEntity(-1, food.user_id, -1, -1, 
+        "ビーフ", d, "rotten");
     };
 
 
     // 食材削除処理
     const foodTrash = async (food: FoodTrash) => {
   
-      // haiki食品情報の取得
-      const id = food.id;
-      const user_id = food.user_id;
+      // // 廃棄食品情報の取得
+      // const id = food.id;
+      // const user_id = food.user_id;
   
       // データベース削除
 
